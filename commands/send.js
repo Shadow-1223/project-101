2208192faf22236dac1bd45e780c92b4ad326731
migzchi <87005004+Shadow-1223@users.noreply.github.com>
2022-08-20 13:36:44 +0800

    Update send.js

--- commands/send.js
+++ commands/send.js
@@ -47,7 +47,7 @@ module.exports = {
     const embed = new MessageEmbed()
       embed.setTitle(title) 
       embed.setDescription(text)
-    interaction.channel.send({ embeds: [embed, embeds] })
+    interaction.channel.send({ embeds: [embed] })
 
 
     if (interaction) {
