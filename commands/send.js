e9d742dbf61b79a38def017bb911fe2c3824340a
migzchi <87005004+Shadow-1223@users.noreply.github.com>
2022-08-20 08:12:11 +0800

    Update send.js

--- commands/send.js
+++ commands/send.js
@@ -44,9 +44,9 @@ module.exports = {
 
     const msg2 = new MessageEmbed()
       .setImage(attachment)
-      .setDescription(text)
 
     const msg1 = new MessageEmbed()
+      .setTitle(text)
       .setDescription(text)
     interaction.channel.send({ embeds: [msg2, msg1] })
 
