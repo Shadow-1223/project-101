87b7edaf4bbef04a91ec9f9dc531ba84ccb68661
migzchi <87005004+Shadow-1223@users.noreply.github.com>
2022-08-29 20:06:48 +0800

    Update send.js

--- commands/send.js
+++ commands/send.js
@@ -1,77 +1,82 @@
-const { Permissions, Constants, MessageEmbed, MessageAttachment } = require("discord.js");
+const { Permissions, Constant, MessageEmbed } = require('discord.js')
 
 module.exports = {
-  name: 'send',
-  description: 'just a copy of send slash command',
-  permissions: [Permissions.FLAGS.ADMINISTRATOR],
-  type: Constants.ApplicationCommandTypes.CHAT_INPUT,
-  options: [
-    {
-      name: 'channel',
-      description: 'mention a channel',
-      type: Constants.ApplicationCommandOptionTypes.CHANNEL,
-      required: true
-    },
-    {
-      name: 'text',
-      description: 'type a message',
-      type: Constants.ApplicationCommandOptionTypes.STRING,
-      required: true
-    },
-    {
-      name: 'attachment',
-      description: 'attach an image',
-      type: Constants.ApplicationCommandOptionTypes.ATTACHMENT,
-      required: false
-    },
-    { 
-      name: 'title',
-      description: 'type a title',
-      type: Constants.ApplicationCommandOptionTypes.STRING,
-      required: false
-    },
-    { 
-      name: 'hexcolor',
-      description: 'put a hexcolor',
-      type: Constants.ApplicationCommandOptionTypes.STRING,
-      required: false
-    }
-  ],
-  slash: true,
-  async execute({ interaction, message, client, options }) {
-    if(message) return message.reply({
-        content : 'this cmd is not working on leagcy cmd',
-        allowedMentions : {
-            repliedUser: false
+    name : 'send',
+    description : 'Send a message',
+    type : Constant.ApplicationCommandTypes.CHAT_INPUT,
+    permissions : [Permissions.FLAGS.ADMINISTRATOR],
+    options : [
+        {
+            name : 'channel',
+            description : 'Specify a channel',
+            type : Constant.ApplicationCommandOptionTypes.CHANNEL,
+            required : true
+        },
+        {
+            name : 'message',
+            description : 'Type a Message in this options',
+            type : Constant.ApplicationCommandOptionTypes.STRING,
+            required : true
+        },
+        {
+            name : 'title',
+            description : 'Set a title in this options and it will show on the MessageEmbeds once it got sent',
+            type : Constant.ApplicationCommandOptionTypes.STRING,
+            required : false
+        },
+        {
+            name : 'attachment',
+            description : 'Put a image from your gallery/album in this options and it will show in the MessageEmbed',
+            type : Constant.ApplicationCommandOptionTypes.ATTACHMENT,
+            required : false
+        },
+        {
+            name : 'hex_color',
+            description : 'Set/Type a hex color in this options and it will show in MessageEmbed',
+            type : Constant.ApplicationCommandOptionTypes.STRING,
+            required : false
         }
-    })
-   
-    const channel = options.getChannel('channel')
-
-    const attachment = options.getAttachment('attachment')
-    const hexcolor = options.getString('hexcolor')
-
-    const text = options.getString('text')
-    const title = options.getString('title')
-    const embeds = [];
-
-    if (attachment) embeds.push(
-         new MessageEmbed()
-             .setImage(attachment.url)
-             .setColor(hexcolor)
-         )
-
-    const embed = new MessageEmbed()
-      if(title) embed.setTitle(title)
-      embed.setDescription(text)
-      try {
-          embed.setColor(hexcolor)
-      } catch {
-          embed.setColor(null)
-      }
-      embeds.push(embed)
-      
-    channel.send({ embeds: embeds })
-    interaction.reply({ content: `message was sent by <@${interaction.user.id}> in ${channel}`, ephemeral: true })
-  }
+    ],
+    
+    slash : true,
+    async execute({ interaction , message , options }) {
+        if(message) return message.reply({
+            content : 'The command does not have legacy command build in. Please try use slash command',
+            allowedMentions : {
+                repliedUser: false
+            }
+        })
+        
+        const channel = options.getChannel('channel')
+        const attachment = options.getAttachment('attachment')
+        const hexColor = options.getString('hex_color')
+        
+        const text = options.getString('message')
+        const title = options.getString('title')
+        const embeds = [];
+        
+        if(attachment) embeds.push(
+            new MessageEmbed()
+               .setImage(attachment.url)
+               .setColor(hexColor)
+        )
+        
+        const embed = new MessageEmbed()
+        if(title) embed.setTitle(title)
+        embed.setDescription(text)
+        try {
+            embed.setColor(hexColor)
+        } catch {
+            embed.setColor(null)
+        }
+        embeds.push(embed)
+        channel.send({ embeds : embeds })
+        
+        if(interaction) {
+            interaction.reply({
+                content : `Your message was sent in ${channel}`,
+                ephemeral : true
+            })
+        }
+    }
 }
