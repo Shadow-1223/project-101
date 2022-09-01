f5a76b7c2382e55c403efcc19c27ccb29fffaeaa
migzchi <87005004+Shadow-1223@users.noreply.github.com>
2022-09-01 13:41:56 +0800

    Update and rename suggestions.js to set-suggestions-channel.js

---
+++ commands/Suggestions/set-suggestions-channel.js
@@ -0,0 +1,23 @@
+const Commando = require('discord.js-commando')
+const { Permissions } = require('discord.js')
+
+module.exports = class SetSuggestionChannelCommand extends Commando.commands {
+    constructor(client) {
+        super(client, {
+            name: 'setsuggestions',
+            group: 'suggestions',
+            memberName: 'setsuggestions',
+            userPermissions: [
+                Permissions.FLAGS.ADMINISTRATOR
+            ],
+            description: 'set the suggestions channel'
+        })
+    }
+    
+    
+    run = async (message) => {
+        const {
+            channel: { id },
+        } = message 
+    }
+}
