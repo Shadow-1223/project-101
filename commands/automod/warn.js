06c376557147eb5988ed0c1e457d5a774a4b6c4b
migzchi <87005004+Shadow-1223@users.noreply.github.com>
2022-09-05 21:11:10 +0800

    Update warn.js

--- commands/automod/warn.js
+++ commands/automod/warn.js
@@ -1,5 +1,5 @@
 const {Permissions, Constants, MessageEmbed} = require('discord.js')
-const WarnModel = require('../models/warnModel')
+const WarnModel = require('../modals/warnModel')
 
 module.exports = {
     name : "warn",
