e6871d37916be920fe09b3910a19ea54cd88ce72
migzchi <87005004+Shadow-1223@users.noreply.github.com>
2022-09-07 21:28:39 +0800

    Update index.js

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ client.on('ready', async () => {
     }
     )
   // Set the client user's activity
-client.user.setActivity('ka help', { type: 'WATCHING' });
+client.user.setActivity('ka help', { type: 'STREAMING' });
 })
 
 sdhandler.sdhandler({
