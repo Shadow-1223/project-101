3f85b35c181133ceb1c89dbb234e9a5458107a49
migzchi <87005004+Shadow-1223@users.noreply.github.com>
2022-09-08 15:54:20 +0800

    Update messageCreate.js

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -3,21 +3,19 @@ const { MessageEmbed }= require('discord.js')
 module.exports = {
     name : 'messageCreate',
     async run(client , message) {
-        client.on('messageCreate', async (message) => {
-            if(message.channel.id === '986920640227459115') {
-                let embed = new MessageEmbed()
-                .setTitle('New Suggestions')
-                .setColor('Yellow')
-                .setAuthor({ name: `${message.author.tag}`, iconURL: `${message.author.displayAvatarURL({ dynamic: true }) }` })
-                .setDescription(`${message.content}`)
-                .setTimestamp()
-                
-                message.channel.send({ embeds: [embed] }).then(async (msg) => {
-                    await message.delete()
-                    msg.react("👍")
-                    msg.react("👎")
-                })
-            }
-        })
+        if(message.channel.id === '986920640227459115') {
+            let embed = new MessageEmbed()
+            .setTitle("New Suggestions")
+            .setColor("Yellow")
+            .setAuthor({ name: `${message.author.tag}`, iconURL: `${message.author.displayAvatarURL({ dynamic: true })}` })
+            .setDiscription(`${message.content}`)
+            .setTimestamp()
+            
+            message.channel.send({ embeds: [embed] }).then(async (msg) => {
+                await message.delete()
+                msg.react("👍")
+                msg.react("👎")
+            })
+        }
     }
 }
