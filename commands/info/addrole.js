ba3f9834c646e6c96f6aa6638ba677f4de151450
migzchi <87005004+Shadow-1223@users.noreply.github.com>
2022-09-14 18:30:20 +0800

    Update addrole.js

--- commands/info/addrole.js
+++ commands/info/addrole.js
@@ -61,7 +61,7 @@ module.exports = {
     },
     
     slash : true,
-    async execute({message , interactionxhnxndndcn.  rcffmvnngncnvnvmmvmvbmmgmg knob kfkfj7, client}) {
+    async execute({message , interaction , client}) {
         if(message) return message.reply({
             content : 'This command does not work on legacy command. Please try use slash command',
             allowedMentions : {
