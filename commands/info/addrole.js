a62dfa846bd0a861811ae95673ca25dc725c49dc
migzchi <87005004+Shadow-1223@users.noreply.github.com>
2022-09-19 13:52:24 +0800

    Update addrole.js

--- commands/info/addrole.js
+++ commands/info/addrole.js
@@ -1,137 +1,152 @@
-const { Constants, Permissions, MessageActionRow, MessageSelectMenu, } = require("discord.js");
+const { Constants, Permissions, MessageActionRow, MessageSelectMenu } = require("discord.js")
+const prefix = 'auto_roles'
 
 module.exports = {
-  name : "addrole",
-  description : "adds a role to the auto role message.",
-  type : Constants.ApplicationCommandTypes.CHAT_INPUT,
-  requiredRoles : ['985756703087788062' , '986179620640550952'],
-  permissions : [Permissions.FLAGS.ADMINISTRATOR],
-  options: [
-    {
-      name: "message_link",
-      description: "Enter message link",
-      type: Constants.ApplicationCommandOptionTypes.STRING,
-      required: true,
-    },
-    {
-      name: "role",
-      description: "type a role name to added it on dropdown menu",
-      type: Constants.ApplicationCommandOptionTypes.ROLE,
-      required: true,
-    },
-  ],
-
-  async init(client) {
-    client.on("interactionCreate", async (interaction) => {
-      if (!interaction.isSelectMenu()) {
-        return;
-      }
-
-      const { customId, values, member } = interaction;
-
-      if (customId === "auto_roles") {
-        const component = interaction.component;
-        const removed = component.options.filter((options) => {
-          return !values.includes(options.value);
-        });
-
-        for (const id of removed) {
-          await member.roles.remove(id.value);
+    name : 'addrole',
+    description : 'add a role to the menu',
+    type : Constants.ApplicationCommandTypes.CHAT_INPUT,
+    permissions : [Permissions.FLAGS.ADMINISTRATOR],
+    requiredRoles : ['985756703087788062' , '986179620640550952'],
+    options : [
+        {
+            name : 'message_link',
+            description : 'Enter message link',
+            type : Constants.ApplicationCommandOptionTypes.STRING,
+            required : true
+        },
+        {
+            name : 'role',
+            description : 'Enter role in this options',
+            type : Comstants.ApplicationCommandOptionTypes.ROLE,
+            required : true
         }
-
-        for (const id of values) {
-          await member.roles.add(id);
-        }
-
-        await interaction.reply({
-          content: "Roles Update!",
-          ephemeral: true,
-        });
-      }
-    });
-  },
-
-  slash: true,
-  async execute({ message, interaction, client }) {
+    ],
     
-    const link = interaction.options.getString("message_link", true);
-    const stuff = link.split("/");
-    const messageID = stuff.pop();
-    const channelID = stuff.pop();
-    const channel = interaction.guild.channels.cache.get(channelID);
+    async init(client) {
+        client.on('interactionCreate', async (interaction) => {
+            if(!interaction.isSelectMenu()) {
+                return;
+            }
+            
+            const { guild, customId } = interaction
+            if(!guild || !customId.startsWith(prefix)) {
+                return;
+            }
+            
+            const roleId = customId.replace(prefix, '')
+            const member = interaction.member
+            
+            if(member.roles.cache.has(roleId)) {
+                member.roles.remove(roleId)
+                
+                interaction.reply({
+                    content : `You no longer have <@&${roleId}> role`
+                })
+            } else {
+                member.roles.add(roleId)
+                
+                interaction.reply({
+                    content : `You now have <@&${roleId}> role`
+                })
+            }
+        })
+    },
     
-    const role = interaction.options.getRole("role", true);
-    if (!role) {
-      return interaction.deferReply({
-          content : "Unknown role", 
-          ephemeral: true
-      })
-    }
-
-    const targetMessage = await channel.messages.fetch(messageID, {
-      cache: true,
-      force: true,
-    });
-
-    if (!targetMessage) {
-      return interaction.deferReply({content : "Unknown messages ID.", ephemeral: true});
-    }
-
-    if (targetMessage.author.id !== client.user?.id) {
-      return interaction.reply ({content : `Please provide a message ID that was sent <@${client.user?.id}>`, ephemeral: true});
-    }
-
-    let row = targetMessage.components[0];
-    if (!row) {
-      row = new MessageActionRow();
-    }
-
-    const options = [
-      {
-        label: role.name,
-        value: role.id,
-      },
-    ];
-
-    let menu = row.components[0];
-    if (menu) {
-      for (const o of menu.options) {
-        if (o.value === options[0].value) {
-          return interaction.reply({
-            content: `<@&${o.value}> is already part of this menu.`,
-            allowMentions: {
-              roles: [],
-            },
-            ephemeral: true
-          })
+    slash : true,
+    async execute({ interaction , message , client }) {
+        if(message) return message.reply({
+            content : 'this command does not have legacy cmd on',
+            allowedMentions : {
+                repliedUser : false
+            }
+        })
+        
+        const linkinteraction.options.getString('message_link', true)
+        const stuff = link.split('/')
+        const messageID = stuff.pop()
+        const channelID = stuff.pop()
+        const channel = interaction.guild.channels.cache.get(channelID)
+        if(!link) return interaction.reply({
+            content : 'invalid link',
+            ephemeral : true
+        })
+        
+        const role = interaction.options.getRole("role", true) 
+        if(!role) {
+            return interaction.reply({
+                content : 'Unknown' role',
+                ephemeral : true
+            })
         }
-      }
-
-      menu.addOptions(options);
-      menu.setMaxValues(menu.options.length);
-    } else {
-      row.addComponents(
-        new MessageSelectMenu()
-          .setCustomId("auto_roles")
-          .setMinValues(0)
-          .setMaxValues(1)
-          .setPlaceholder("select your roles")
-          .addOptions(options)
-      );
+        
+        const targetMessage = await channel.messages.fetch(messageID, {
+            cache : true,
+            force : true
+        })
+        
+        if(!targetMessage) return interaction.reply({
+            content : 'Unknown message ID',
+            ephemeral : true
+        })
+        
+        if(targetMessage.author.id !== client.user?.id) {
+            return interaction.reply({
+                content : `Please provide a message ID that was sent ${client.user?.id}`,
+                ephemeral: true
+            })
+        }
+        
+        let row = targetMessage.components[0]
+        if(!row) {
+            row = new MessageActionRow()
+        }
+        
+        const options = [
+            {
+                label : role.name,
+                value : role.id
+            }
+        ]
+        
+        let menu = row.components[0]
+        if(menu) {
+            for (const o of menu.options) {
+                if(o.value === options[0].value) {
+                    return interaction.reply({
+                        content : `<@&${o.value}> is already part of this menu`,
+                        allowedMentions : {
+                            roles : [],
+                        },
+                        ephemeral: true
+                    })
+                }
+            }
+            
+            menu.addOptions(options)
+            menu.setMaxValues(menu.options.length)
+        } else {
+            row.addComponents(
+                new MessageSelectMenu()
+                .setCustomId(`${prefix}${roleId}`)
+                .setMinValues(0)
+                .setMaxValues(1)
+                .setPlaceholder("select your role")
+                .addOptions(options)
+            )
+        }
+        
+        targetMessage.edit({
+            components : [row]
+        })
+        
+        if(interaction) {
+            interaction.reply({
+                content : `added <@&${role.id}> to the menu`,
+                allowedMentions : {
+                    roles : []
+                },
+                ephemeral : true
+            })
+        }
     }
-
-    targetMessage.edit({
-      components: [row]
-    });
-
-    if(interaction) {
-        interaction.reply({
-          content: `added <@&${role.id}> to the auto roles now.`,
-          allowMentions: {
-             roles: [],
-          },
-         ephemeral: true
-      })
-    }
-  },
-};
+}
