ca845629468acaaef0e1e82806eac349ac89321a
migzchi <87005004+Shadow-1223@users.noreply.github.com>
2022-10-01 20:48:40 +0800

    Update ping.js

--- commands/info/ping.js
+++ commands/info/ping.js
@@ -5,8 +5,7 @@ module.exports = new Command({
     name : "ping", // Name of the command
     description : "Pings the sender!", // Brief description of the command
     mode : CommandMode.Both, // Mode of the command - Legacy , Slash or Both
-    type : 
-    async execute({message , interaction , member}){ // Command callback function
+    async execute({ message , interaction , member }) { // Command callback function
         if(message) return message.reply({content : `Hey , ${member}`})
 
         else {
