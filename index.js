1f0eee352718e6d2c1b818b700201e12fe2e1732
migzchi <87005004+Shadow-1223@users.noreply.github.com>
2022-10-09 21:56:30 +0800

    Update index.js with better format
    and add more intents

--- index.js
+++ index.js
@@ -1,34 +1,36 @@
-require('dotenv').config();
+require('dotenv/config')
 
 const { SDClient } = require('sdhandler')
 const mongoose = require('mongoose')
-
 const { Intents, Client } = require('discord.js')
 
 const client = new Client({
-  // These intents are recommended for the built in help menu
-  intents : [
-	Intents.FLAGS.GUILDS,
-	Intents.FLAGS.GUILD_MESSAGES,
-        Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
-	Intents.FLAGS.GUILD_PRESENCES
-  ],
+    intents : [
+        Intents.FLAGS.GUILDS,
+        Intents.FLAGS.GUILD_MESSAGES,
+        Intents.FLAGS.GUILD_MESSAGES_REACTIONS,
+        Intents.FLAGS.GUILD_PRESENCE,
+        Intents.FLAGS.MESSAGE_CONTENT,
+        Intents.FLAGS.GUILD_BANS,
+        Intents.FLAGS.GUILD_MEMBERS,
+        Intents.FLAGS.GUILD_EMOJI_AND_STICKERS,
+    ]
 })
 
-client.on('ready', async () => {
-  await mongoose.connect(
-    process.env.MONGO_URI,
-    {
-      keepAlive: true
-    }
+client.on("ready", async () {
+    await mongoose.connect(
+        process.env.MONGO_URI,
+        {
+            keepAlive : true
+        }
     )
-  // Set the client user's activity
-client.user.setActivity('ka help', { type: 'WATCHING' });
+    // set the bot activities
+    client.user.setActivity("ka help (currently in maintenance please wait until we fix it the problem)", { type : "STREAMING" })
 })
 
 new SDClient({
     token : process.env.TOKEN, // Your Bot's token
-    intents : [client] , // An Array of Intents
+    intents : client.options.intents , // An Array of Intents
     commandsDir : "./commands", // The directory which contains command files .Default is "commands"
     eventsDir : "./events", // The directory which contains event files .Default is "events"
     testOnly : false, // Toggle this to true if you want to register in specific test servers . Default is "false"
