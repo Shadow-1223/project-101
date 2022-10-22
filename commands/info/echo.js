e23e4e77dfc81f90f6d04498d3efce16262383aa
migzchi <87005004+Shadow-1223@users.noreply.github.com>
2022-10-22 22:57:17 +0800

    Update echo.js

--- commands/info/echo.js
+++ commands/info/echo.js
@@ -1,29 +1,40 @@
-const { Permissions, Constants } = require("discord.js");
+const { Permissions , Constants } = require("discord.js")
 
 module.exports = {
-    name : 'echo',
-    description : 'Echoes your words.',
-    permissions : [Permissions.FLAGS.SEND_MESSAGES],
-    type : Constants.ApplicationCommandTypes.CHAT_INPUT,
-    aliases : ['tell' , 'say'],
+    name : "echo" ,
+    description : "Echoes your words" ,
+    permissions : [Permissions.FLAGS.SEND_MESSAGES] ,
+    type : Constants.ApplicationCommandTypes.CHAT_INPUT ,
+    aliases : ["tell" , "say"] ,
+    slash : true
     options : [
         {
-            name : 'text' , 
-            description : 'Text',
-            type : Constants.ApplicationCommandOptionTypes.STRING,
-            required : true
+            name : "message" ,
+            description : "Write a words" ,
+            required : true ,
+            type : Constants.ApplicationCommandOptionTypes.STRING
         }
     ],
-    slash : true,
-    async execute({interaction , options , args , message}){
-        if(message){
-            const text = args.join(' ')
-            if(!text) return message.reply({content : "You need to specify what you wanna Echo."})
-            await message.reply({content : `You said **${text}**`})
+    
+    async execute({ interaction , message , options , args }) {
+        if(message) {
+            const text = args.join(" ")
+            if(!text) return message.reply({
+                content : "You need to specify what words you wanna echo" ,
+                allowedMentions : {
+                    repliedUser : false
+                }
+            })
+            await message.reply({
+                content : `You said **${text}**`
+            })
         }
-        if(interaction){
-            const text = options.getString('text')
-            await interaction.reply({content : `You said **${text}**`})
+        
+        if(interaction) {
+            const text = options.getString("message")
+            await interaction.reply({
+                content : `You said **${text}**`
+            })
         }
     }
 }
