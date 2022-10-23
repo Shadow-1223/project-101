6e9bf5ad2614db83edfb513d1e3add8f1a953578
migzchi <87005004+Shadow-1223@users.noreply.github.com>
2022-10-23 21:54:53 +0800

    Update voiceStateUpdate.js

--- events/voiceStateUpdate.js
+++ events/voiceStateUpdate.js
@@ -1,21 +1,25 @@
-const client = require("../index.js")
-const { Collection } = require("discord.js")
-const { Event } = require("sdhandler")
+const { Collection , Client } = require("discord.js")
 const voiceCollection = new Collection()
 
-module.exports = new Event("voiceStateUpdate", async ( oldState , newState ) => {
-    const user = await client.users.fetch(newState.id)
-    const member = newState.guild.member(user)
-    
-    if(!oldState.channel && newState.channel.id === "1030821612427149363") {
-        const channel = await newState.guild.channels.create(`${user.username}'s channel`, {
-            type : "voice",
-            parent : newState.channel.parent
-        })
-        member.voice.setChannel(channel)
-        voiceCollection.set(user.id , channel.id)
-    } else if(!newState.channel) {
-        if(oldState.channelID === voiceCollection.get(newState.id))
-          return oldState.channel.delete()
+module.exports = {
+    name : "voiceStateUpdate" ,
+    /**
+     * @param ${Client} client
+    */
+    async run(client , oldState , newState ) {
+        const user = await client.users.fetch(newState.id)
+        const member = newState.guild.member(user)
+        
+        if(!oldState.channel && newState.channel.id === "1030821612427149363") {
+            const channel = await newState.guild.channels.create(`${user.username}'s channel` , {
+                type : "voice" ,
+                parent : newState.channel.parent
+            })
+            
+            member.voice.setChannel(channel)
+            voiceCollection.set(user.id , channel.id)
+        } else if(!newState.channel) {
+            if(oldState.channelID === voiceCollection.get(newState.id)) return oldState.channel.delete()
+        }
     }
-})
+}
