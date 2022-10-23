d8dd713650060607e214a3b1d87b9c0c9bd2c429
migzchi <87005004+Shadow-1223@users.noreply.github.com>
2022-10-23 10:38:35 +0800

    Update ping.js

--- commands/info/ping.js
+++ commands/info/ping.js
@@ -1,15 +1,23 @@
-const { Permissions, Constants } = require("discord.js");
+const { Permissions , Constants , MessageEmbed } = require("discord.js")
 
 module.exports = {
-    name : 'ping',
-    description : 'Replies with a pong !',
-    permissions : [Permissions.FLAGS.SEND_MESSAGES],
-    type : Constants.ApplicationCommandTypes.CHAT_INPUT,
-    slash : true,
-    async execute({interaction}){
-
-        if(interaction){
-            await interaction.reply({content : 'Pong !'})
+    name : "ping" ,
+    description : "Replied with a pong!" ,
+    permissions : [Permissions.FLAGS.SEND_MESSAGES] ,
+    type : Constants.ApplicationCommandTypes.CHAT_INPUT ,
+    aliases : ["p"] ,
+    slash : true ,
+    
+    async execute({ interaction , args , messages , client }) {
+        if(interaction) {
+            const ping =  new Date().getTime() - interaction.createdTimestamp
+            const time = Math.round(client.ws.ping)
+            
+            const pingEmbed = new MessageEmbed()
+            .setTitle("🏓 Pong!")
+            .setDescription("**Lentacy:** \n " + `${ping}ms` + "\n **API** \n")
+            
+            interaction.reply({ embeds : pingEmbed })
         }
     }
 }
