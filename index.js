f4763858f01e473f19abecbc906a18a2948bc667
migzchi <87005004+Shadow-1223@users.noreply.github.com>
2022-11-03 21:49:10 +0800

    Update index.js

--- index.js
+++ index.js
@@ -8,11 +8,12 @@ const client = new Client({
     intents : [
         Intents.FLAGS.GUILDS ,
         Intents.FLAGS.GUILD_MESSAGES ,
-        Intents.FLAGS.FLAGS.GUILD_MESSAGE_REACTIONS ,
+        Intents.FLAGS.GUILD_MESSAGE_REACTIONS ,
         Intents.FLAGS.GUILD_PRESENCES ,
         Intents.FLAGS.GUILD_BANS,
         Intents.FLAGS.GUILD_MEMBERS ,
         Intents.FLAGS.GUILD_EMOJIS_AND_STICKERS ,
+        Intents.FLAGS.GUILD_VOICE_STATES
     ]
 })
 
