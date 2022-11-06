f40fbee3e8de8fa2cfb932cbf323ee033a3a4923
migzchi <87005004+Shadow-1223@users.noreply.github.com>
2022-11-06 10:28:01 +0800

    Fix something

--- commands/info/anime.js
+++ commands/info/anime.js
@@ -1,93 +1,85 @@
-const { MessageEmbed , Permissions , Constants } = require("discord.js")
-const anime = require("../../utils/anime.js")
-
+const { MessageEmbed , Permissions , Constants } = require('discord.js');
+const anime = require('../../utils/anime');
 module.exports = {
-    name : "anime_finder",
-    description : "Find anime by name or ID",
+    name: 'anime_finder',
+    description: 'Find Anime by name or id',
     permissions : [Permissions.FLAGS.SEND_MESSAGES],
+    type : Constants.ApplicationCommandTypes.CHAT_INPUT,
     slash : true,
-    options : [
+    options: [
         {
-            name : "anime",
-            description : "Please Enter anime name or ID",
-            type : Constants.ApplicationCommandOptionTypes.STRING,
-            required : true
+            name: 'anime',
+            description: 'Please Enter the Anime Name or ID',
+            required: true,
+            type: Constants.ApplicationCommandOptionTypes.STRING
         }
-    ] ,
-    
-    async execute({ interaction , client , options }) {
-        let animeEmbed = new MessageEmbed()
-        .setTitle("Finding Anime...")
-        .setDescription("Please Wait...")
-        .setColor("#0099ff")
-        
+    ],
+    execute: async (interaction, client, options) => {
+        let embed = new MessageEmbed()
+        .setTitle('Finding Anime...')
+        .setDescription('Please Wait...')
+        .setColor('#0099ff')
+
         let message = await interaction.reply({
-            embeds : [animeEmbed],
-            fetchReply : true
+            embeds: [embed],
+            fetchReply: true
         })
         
         let animeId = parseInt(options[0].value);
         let animeFinder = await new anime(client)
         if(!isNaN(animeId)) {
             try {
-                console.log(animeId);
-                let foundAnime = await animeFinder.getAnimeById(animeId);
-                if(!foundAnime) {
-                    animeEmbed.title = "Anime Not Found."
-                    animeEmbed.description = "Please Try Again."
-                    animeEmbed.color = "#ff0000"
-                    return message.edit({
-                        embeds : [animeEmbed] 
-                    })
-                }
-                animeEmbed.title = foundAnime.title
-                animeEmbed.description = foundAnime.description
-                animeEmbed.color = "#0099ff"
-                animeEmbed.setThumbnail(foundAnime.image)
-                animeEmbed.addField("Type", foundAnime.type, true)
-                animeEmbed.addField("Episode", foundAnime.episodes.toString(), true)
-                animeEmbed.addField("Status", foundAnime.statistics.status, true)
-                animeEmbed.addField("Score", foundAnime.statistics.score.toString(), true)
-                animeEmbed.addField("Scored by", foundAnime.statistics.scored_by.toString(), true)
-                animeEmbed.addField("Rank", foundAnime.statistics.rank.toString(), true)
-                animeEmbed.addField("Popularity", foundAnime.statistics.popurlarity.toString(), true)
-                animeEmbed.addField("Members", foundAnime.statistics.member.toString(), true)
-                animeEmbed.addField("Favorites", foundAnime.statistics.favorites.toString(), true)
-                message.edit({
-                    embeds : [animeEmbed],
-                    fetchReply : true
-                })
-            } catch(err) { console.log(err) }
+            console.log(animeId);
+            let foundAnime = await animeFinder.getAnimeById(animeId);
+            if(!foundAnime) {
+                embed.title = 'Anime Not Found';
+                embed.description = 'Please Try Again';
+                embed.color = '#ff0000';
+                return message.edit({embeds: [embed]});
+            }
+            embed.title = foundAnime.title;
+            embed.description = foundAnime.description;
+            embed.color = '#0099ff';
+            embed.setThumbnail(foundAnime.image);
+            embed.addField('Type', foundAnime.type, true);
+            embed.addField('Episodes', foundAnime.episodes.toString(), true);
+            embed.addField('Status', foundAnime.status, true);
+            embed.addField('Score', foundAnime.statistics.score.toString(), true);
+            embed.addField('Scored By', foundAnime.statistics.scored_by.toString(), true);
+            embed.addField('Rank', foundAnime.statistics.rank.toString(), true);
+            embed.addField('Popularity', foundAnime.statistics.popularity.toString(), true);
+            embed.addField('Members', foundAnime.statistics.member.toString(), true);
+            embed.addField('Favorites', foundAnime.statistics.favorites.toString(), true);
+            message.edit({ embeds: [embed], fetchReply: true })
+        } catch(err) { console.log(err) } 
         } else {
             try {
-                let foundAnime = await animeFinder.getAnimeByName(options[0].value)
+                let foundAnime = await animeFinder.getAnimeByName(options[0].value);
                 if(!foundAnime) {
-                    animeEmbed.title = "Anime Not Found"
-                    animeEmbed.description = "Please Try Again"
-                    animeEmbed.color = "#ff0000"
-                    return message.edit({ embeds : [animeEmbed] })
+                    embed.title = 'Anime Not Found';
+                    embed.description = 'Please Try Again';
+                    embed.color = '#ff0000';
+                    return message.edit({embeds: [embed]});
                 }
-                
+                // Check to see if it is a array
                 if(Array.isArray(foundAnime)) {
-                    foundAnime = foundAnime[0]
+                    foundAnime = foundAnime[0];
                 }
-                console.log(foundAnime)
-                animeEmbed.title = foundAnime.title
-                animeEmbed.description = foundAnime.description
-                animeEmbed.color = "#0099ff"
-                animeEmbed.addField("Type", foundAnime.type, true)
-                animeEmbed.addField("Episode", foundAnime.episodes.toString(), true)
-                animeEmbed.addField("Status", foundAnime.statistics.status, true)
-                animeEmbed.addField("Score", foundAnime.statistics?.score.toString() || foundAnime.score.toString(), true)
-                animeEmbed.addField("Scored by", foundAnime.statistics?.scored_by.toString() || foundAnime.scored_by.toString(), true)
-                animeEmbed.addField("Rank", foundAnime.statistics?.rank.toString() || foundAnime.rank.toString(), true)
-                animeEmbed.addField("Popularity", foundAnime.statistics?.popurlarity.toString() || foundAnime.popularity.toString(), true)
-                animeEmbed.addField("Members", foundAnime.statistics?.member.toString() || foundAnime.member.toString(), true)
-                animeEmbed.addField("Favorites", foundAnime.statistics?.favorites.toString() || foundAnime.favorites.toString(), true)
-                message.edit({
-                    embeds : [animeEmbed],
-                    fetchReply : true
-                })
+                console.log(foundAnime);
+                embed.title = foundAnime.title;
+                embed.description = foundAnime.description;
+                embed.color = '#0099ff';
+                embed.setThumbnail(foundAnime.image);
+                embed.addField('Type', foundAnime.type, true);
+                embed.addField('Episodes', foundAnime.episodes.toString(), true);
+                embed.addField('Status', foundAnime.status, true);
+                embed.addField('Score', foundAnime.statistics?.score.toString() || foundAnime.score.toString(), true);
+                embed.addField('Scored By', foundAnime.statistics?.scored_by.toString() ||  foundAnime.scored_by.toString(), true);
+                embed.addField('Rank', foundAnime.statistics?.rank.toString() ||  foundAnime.rank.toString() , true);
+                embed.addField('Popularity', foundAnime.statistics?.popularity.toString() ||  foundAnime.popularity.toString() , true);
+                embed.addField('Members', foundAnime.statistics?.member.toString() ||  foundAnime.member.toString() , true);
+                embed.addField('Favorites', foundAnime.statistics?.favorites.toString() ||  foundAnime.favorites.toString() , true);
+                message.edit({ embeds: [embed], fetchReply: true })
             } catch(err) { console.log(err) }
         }
     }
