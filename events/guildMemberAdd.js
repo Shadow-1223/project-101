025c6791b70c3328558299fb70f9c3a6be5ad98e
migzchi <87005004+Shadow-1223@users.noreply.github.com>
2022-11-20 16:03:46 +0800

    Update guildMemberAdd.js

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,5 +1,6 @@
 const { MessageAttachment } = require("discord.js")
 const Canvas = require("canvas")
+Canvas.registerFont("./Fonts/JetBrainsMono-Regular.ttf", { family : "JetBrains Mono" })
 
 module.exports = {
     name : "guildMemberAdd" ,
@@ -7,7 +8,7 @@ module.exports = {
         const welcomeCanvas = {};
         welcomeCanvas.create = Canvas.createCanvas(1024, 500)
         welcomeCanvas.context = welcomeCanvas.create.getContext('2d')
-        welcomeCanvas.context.font = '72px sans-serif';
+        welcomeCanvas.context.font = '72px "JetBrains Mono"';
         welcomeCanvas.context.fillStyle = '#ffffff';
         
         Canvas.loadImage("./structures/Images/Welcomebg.jpg").then(async (img) => {
@@ -21,10 +22,10 @@ module.exports = {
         
         const welcomechannel = client.channels.cache.get('981108742714196051')
         let canvas = welcomeCanvas;
-        canvas.context.font = '42px Roboto Medium',
+        canvas.context.font = '42px "JetBrains Mono"',
         canvas.context.textAlign = 'center';
         canvas.context.fillText(member.user.tag.toUpperCase(), 512, 410)
-        canvas.context.font = '32px sans serif'
+        canvas.context.font = '32px "JetBrains Mono"'
         canvas.context.fillText(`You are the ${member.guild.memberCount.toLocaleString()}th`, 512, 455)
         canvas.context.beginPath()
         canvas.context.arc(512, 166, 119, 0, Math.PI * 2, true)
