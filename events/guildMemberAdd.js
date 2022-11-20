491707c34001af1a6efc6fb41a9b17b6976daa93
migzchi <87005004+Shadow-1223@users.noreply.github.com>
2022-11-21 07:14:31 +0800

    Update guildMemberAdd.js

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,6 +1,6 @@
 const { MessageAttachment } = require("discord.js")
 const Canvas = require("canvas")
-Canvas.registerFont('../font/OpenSans-Bold.ttf', { family : 'Open Sans' })
+Canvas.registerFont('./font/OpenSans-Bold.ttf', { family : 'Open Sans', weight : "bold" })
 
 module.exports = {
     name : "guildMemberAdd" ,
@@ -8,7 +8,7 @@ module.exports = {
         const welcomeCanvas = {};
         welcomeCanvas.create = Canvas.createCanvas(1024, 500)
         welcomeCanvas.context = welcomeCanvas.create.getContext('2d')
-        welcomeCanvas.context.font = '72px Open Sans';
+        welcomeCanvas.context.font = 'bold 72px Open Sans';
         welcomeCanvas.context.fillStyle = '#ffffff';
         
         Canvas.loadImage("./Images/Welcomebg.jpg").then(async (img) => {
@@ -22,10 +22,10 @@ module.exports = {
         
         const welcomechannel = client.channels.cache.get('981108742714196051')
         let canvas = welcomeCanvas;
-        canvas.context.font = '42px Open Sans',
+        canvas.context.font = 'bold 42px Open Sans',
         canvas.context.textAlign = 'center';
         canvas.context.fillText(member.user.tag.toUpperCase(), 512, 410)
-        canvas.context.font = '32px Open Sans'
+        canvas.context.font = 'bold 32px Open Sans'
         canvas.context.fillText(`You are the ${member.guild.memberCount.toLocaleString()}th`, 512, 455)
         canvas.context.beginPath()
         canvas.context.arc(512, 166, 119, 0, Math.PI * 2, true)
