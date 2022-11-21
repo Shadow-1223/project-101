430f0b248e6b5e0287d3c6df96b0c3c6a4c1f429
migzchi <87005004+Shadow-1223@users.noreply.github.com>
2022-11-21 18:44:52 +0800

    Update guildMemberAdd.js

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -4,7 +4,7 @@ const Canvas = require("canvas")
 
 function fontFile(name) {
     return path.join(__dirname , "font" , name)
-    console.log(path.join(__dirname , "font" , name))
+    console.log(path.join(__dirname , ".." , "font" , name))
 
 }
 
