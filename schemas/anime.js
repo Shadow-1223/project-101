1ea634ed823ed8f4293302d1d00f4be64500463e
migzchi <87005004+Shadow-1223@users.noreply.github.com>
2022-12-10 18:14:17 +0800

    Update anime.js

--- schemas/anime.js
+++ schemas/anime.js
@@ -3,39 +3,27 @@ const mongoose = require("mongoose")
 
 const animeSchema = new Schema({
     _id : String,
-    anime : [
-        {
-            title : String,
-            englishTitle : String,
-            type : String,
-            image : String,
-            source : String,
-            episodes : Number,
-            status : String,
-            statistics : {
-                score : 0,
-                scored_by : 0,
-                rank : 0,
-                popularity : 0,
-                member : 0,
-                favorites : 0,
-            },
-            airing : Boolean,
-            aired : {
-                from : String,
-                to : String
-            },
-            characters : [
-                {
-                    id : String,
-                    name : String,
-                    image : String,
-                    role : String,
-                    url : String
-                }
-            ]
-        }
-    ]
+    title : String,
+    englishTitle : String,
+    description : String,
+    type : String,
+    image : String,
+    source : String,
+    episodes : Number,
+    status : String,
+    statistics : {
+        score : 0,
+        scored_by : 0,
+        rank : 0,
+        popularity : 0,
+        member : 0,
+        favorites : 0,
+    },
+    airing : Boolean,
+    aired : {
+        from : String,
+        to : String
+    },
 })
 
 module.exports = mongoose.model("anime", animeSchema)
