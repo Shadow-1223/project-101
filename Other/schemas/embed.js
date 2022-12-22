db001c6a48ec17b199ba68ae3b9aa087d32067c9
migzchi <87005004+Shadow-1223@users.noreply.github.com>
2022-12-22 20:22:37 +0800

    Update embed.js

--- Other/schemas/embed.js
+++ Other/schemas/embed.js
@@ -1,9 +1,9 @@
 const { Schema , model } = require("mongoose")
 
 module.exports = model("EmbedBuilder", new Schema({
+    channelID : String,
     description : String,
     title : String,
     image : String,
     hexColor : String,
-    
 }))
