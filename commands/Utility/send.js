e64b8eeccd42dca6494c966f90a4b2debc6d69d9
migzchi <87005004+Shadow-1223@users.noreply.github.com>
2023-01-03 10:24:42 +0800

    Update send.js

--- commands/Utility/send.js
+++ commands/Utility/send.js
@@ -1,5 +1,5 @@
 const { MessageAttachment, MessageEmbed, MessageActionRow, Modal, TextInputComponent, Permissions, Constants } = require("discord.js")
-const EmbedBuilder = require("./Other/schemas/embed.js")
+const EmbedBuilder = require("../../Other/schemas/embed.js")
 
 module.exports = {
     name : "embeds" ,
@@ -128,7 +128,7 @@ module.exports = {
             const channel = interaction.guild.channels.cache.get(channelID)
             const invalid = "```\n" + link + "```"
             if(!link && !channel) return interaction.reply({
-                content : `There is an invalid link you sent. \n ${inavlid}`
+                content : `There is an invalid link you sent. \n ${invalid}`
             })
             
             const targetMessage = await channel.messages.fetch(messageID, {
