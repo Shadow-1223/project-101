e3ffb5a49ffddcf2639b0cdd82ed83821c58c0de
migzchi <87005004+Shadow-1223@users.noreply.github.com>
2023-01-08 21:49:55 +0800

    Made some changes

--- commands/Utility/send.js
+++ commands/Utility/send.js
@@ -104,7 +104,7 @@ module.exports = {
         
         const query = interaction.options.getSubcommand()
         const filter = i => i.customId === "errDel";
-        const collector = interaction.channel.createMessageComponentCollector({ filter, time : 20_000 })
+        const collector = interaction.channel.createMessageComponentCollector({ filter, time : 20000 })
         collector.on("collect", async i => {
             await i.update({ embeds : [] , components : [] })
         })
